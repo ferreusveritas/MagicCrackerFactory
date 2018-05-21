2a97cbb6ba986df3d8be9f9fa9e04f39099fccfb
ferreusveritas <ferreusveritas@gmail.com>
2018-05-21 16:02:31 -0400

    auto

--- src/main/java/com/ferreusveritas/mcf/ModConstants.java
+++ src/main/java/com/ferreusveritas/mcf/ModConstants.java
@@ -3,7 +3,7 @@ package com.ferreusveritas.mcf;
 public class ModConstants {
 
 	public static final String MODID = "mcf";
-	//public static final String VERSION = "@VERSION@";
-	public static final String VERSION = "1.12.2-0.0.1";
+	public static final String VERSION = "@VERSION@";
+	//public static final String VERSION = "1.12.2-0.0.1";
 	
 }
