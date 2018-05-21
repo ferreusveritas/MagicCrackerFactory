b95d49b830b36b2dd7471cd9c45c93a6701faeda
ferreusveritas <ferreusveritas@gmail.com>
2018-05-21 16:02:02 -0400

    fix modid

--- src/main/java/com/ferreusveritas/mcf/ModConstants.java
+++ src/main/java/com/ferreusveritas/mcf/ModConstants.java
@@ -2,8 +2,8 @@ package com.ferreusveritas.mcf;
 
 public class ModConstants {
 
-	public static final String MODID = "stargarden";
+	public static final String MODID = "mcf";
 	//public static final String VERSION = "@VERSION@";
-	public static final String VERSION = "1.12.2-1.0.0";
+	public static final String VERSION = "1.12.2-0.0.1";
 	
 }
