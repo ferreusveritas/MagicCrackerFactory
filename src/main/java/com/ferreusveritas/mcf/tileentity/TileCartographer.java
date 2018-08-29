0b88b5b1df0ba691f6516c37adafeda70ff1142f
ferreusveritas <ferreusveritas@gmail.com>
2018-08-29 18:32:49 -0400

    Add map copy and map swap functions to cartographer

--- src/main/java/com/ferreusveritas/mcf/tileentity/TileCartographer.java
+++ src/main/java/com/ferreusveritas/mcf/tileentity/TileCartographer.java
@@ -1,5 +1,8 @@
 package com.ferreusveritas.mcf.tileentity;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.ferreusveritas.mcf.util.CommandManager;
 import com.ferreusveritas.mcf.util.MethodDescriptor;
 import com.ferreusveritas.mcf.util.MethodDescriptor.SyncProcess;
@@ -12,6 +15,7 @@ import net.minecraft.network.play.server.SPacketMaps;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.storage.MapData;
+import net.minecraft.world.storage.MapDecoration;
 
 public class TileCartographer extends MCFPeripheral  {
 	
@@ -30,6 +34,8 @@ public class TileCartographer extends MCFPeripheral  {
 		setMapScale("n", "scale", (world, peri, args) -> obj(getTool(peri).getCurrMapData().scale = (byte) MathHelper.clamp(args.i(), 0, 4)) ),
 		getMapDimension("", "",	(world, peri, args) -> obj(getTool(peri).getCurrMapData().dimension) ),
 		setMapDimension("n", "dimension", (world, peri, args) -> obj(getTool(peri).getCurrMapData().dimension = args.i()) ),
+		copyMapData("nn", "mapA, mapB", (world, peri, args) -> obj(getTool(peri).copyMapData(args.i(0), args.i(1))) ),
+		swapMapData("nn", "mapA, mapB", (world, peri, args) -> obj(getTool(peri).swapMapData(args.i(0), args.i(1))) ),
 		updateMap("", "", (world, peri, args) -> obj(getTool(peri).updateMap()) ),
 		getBlockMapColor("nnn", "xCoord, yCoord, zCoord", (world, peri, args) -> obj(getTool(peri).getBlockMapColor(args.p())) ),
 		getRGBfromMapColor("n", "index", (world, peri, args) -> getRGBfromMapColor(args.i()) );
@@ -94,6 +100,77 @@ public class TileCartographer extends MCFPeripheral  {
 		getCurrMapData().xCenter = x;
 		getCurrMapData().zCenter = z;
 		return mapNum;
+	}
+
+	public int copyMapData(int mapSrc, int mapDst) {
+
+		MapData mapDataSrc = (MapData) world.loadData(MapData.class, "map_" + mapSrc);
+		MapData mapDataDst = (MapData) world.loadData(MapData.class, "map_" + mapDst);
+		
+		if(mapDataSrc == null || mapDataDst == null) {
+			return -1;
+		}
+		
+		//Copy all of the map data values
+		mapDataDst.xCenter = mapDataSrc.xCenter;
+		mapDataDst.zCenter = mapDataSrc.zCenter;
+		mapDataDst.dimension = mapDataSrc.dimension;
+		mapDataDst.trackingPosition = mapDataSrc.trackingPosition;
+		mapDataDst.unlimitedTracking = mapDataSrc.unlimitedTracking;
+		mapDataDst.scale = mapDataSrc.scale;
+		mapDataDst.colors = mapDataSrc.colors.clone();
+
+		mapDataDst.mapDecorations.clear();
+		for(Entry<String, MapDecoration> entry : mapDataSrc.mapDecorations.entrySet()) {
+			mapDataDst.mapDecorations.put(entry.getKey(), entry.getValue());	
+		}
+
+		mapDataDst.markDirty();
+		Packet<?> packetDst = new SPacketMaps(mapDst, mapDataDst.scale, mapDataDst.trackingPosition, mapDataDst.mapDecorations.values(), mapDataDst.colors, 0, 0, 128, 128);
+		
+		for(EntityPlayer player : world.playerEntities) {
+			if(player instanceof EntityPlayerMP) {
+				((EntityPlayerMP)player).connection.sendPacket(packetDst);
+			}
+		}
+		
+		return 0;
+	}
+
+	
+	public int swapMapData(int mapNumA, int mapNumB) {
+
+		MapData mapDataA = (MapData) world.loadData(MapData.class, "map_" + mapNumA);
+		MapData mapDataB = (MapData) world.loadData(MapData.class, "map_" + mapNumB);
+		
+		if(mapDataA == null || mapDataB == null) {
+			return -1;
+		}
+		
+		//Swap all of the map data values between A and B
+		{ int temp = mapDataA.xCenter; mapDataA.xCenter = mapDataB.xCenter; mapDataB.xCenter = temp; }
+		{ int temp = mapDataA.zCenter; mapDataA.zCenter = mapDataB.zCenter; mapDataB.zCenter = temp; }
+		{ int temp = mapDataA.dimension; mapDataA.dimension = mapDataB.dimension; mapDataB.dimension = temp; }
+		{ boolean temp = mapDataA.trackingPosition; mapDataA.trackingPosition = mapDataB.trackingPosition; mapDataB.trackingPosition = temp; }
+		{ boolean temp = mapDataA.unlimitedTracking; mapDataA.unlimitedTracking = mapDataB.unlimitedTracking; mapDataB.unlimitedTracking = temp; }
+		{ byte temp = mapDataA.scale; mapDataA.scale = mapDataB.scale; mapDataB.scale = temp; }
+		{ byte[] temp = mapDataA.colors; mapDataA.colors = mapDataB.colors; mapDataB.colors = temp; }
+		{ Map<String, MapDecoration> temp = mapDataA.mapDecorations; mapDataA.mapDecorations = mapDataB.mapDecorations; mapDataB.mapDecorations = temp; }
+
+		mapDataA.markDirty();
+		mapDataB.markDirty();
+		
+		Packet<?> packetA = new SPacketMaps(mapNumA, mapDataA.scale, mapDataA.trackingPosition, mapDataA.mapDecorations.values(), mapDataA.colors, 0, 0, 128, 128);
+		Packet<?> packetB = new SPacketMaps(mapNumB, mapDataB.scale, mapDataB.trackingPosition, mapDataB.mapDecorations.values(), mapDataB.colors, 0, 0, 128, 128);
+		
+		for(EntityPlayer player : world.playerEntities) {
+			if(player instanceof EntityPlayerMP) {
+				((EntityPlayerMP)player).connection.sendPacket(packetA);
+				((EntityPlayerMP)player).connection.sendPacket(packetB);
+			}
+		}
+		
+		return 0;
 	}
 	
 	public int updateMap() {
