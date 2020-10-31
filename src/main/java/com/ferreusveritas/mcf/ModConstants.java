df769cacea6acf64b9ff0251af9f49baced66970
Ferreusveritas <ferreusveritas@gmail.com>
2020-10-31 13:50:53 -0400

    Require Baubles

--- src/main/java/com/ferreusveritas/mcf/ModConstants.java
+++ src/main/java/com/ferreusveritas/mcf/ModConstants.java
@@ -6,9 +6,15 @@ public class ModConstants {
 	public static final String VERSION = "1.12.2-9999.9999.9999z";//Maxed out version to satisfy dependencies during dev, Assigned from gradle during build, do not change
 	
 	public static final String REQAFTER = "required-after:";
+	public static final String NEXT = ";";
 	
 	//Other Mods
 	public static final String COMPUTERCRAFT = "computercraft";
+	public static final String BAUBLES = "baubles";
 	
-	public static final String DEPENDENCIES	= REQAFTER + COMPUTERCRAFT;
+	public static final String DEPENDENCIES
+		= REQAFTER + COMPUTERCRAFT
+		+ NEXT
+		+ REQAFTER + BAUBLES;
+	
 }
